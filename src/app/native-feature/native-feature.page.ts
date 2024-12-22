d5e6a9a7724817697a293bc9592763f2743a0ff8
xavierkno <130648848+xavierkno@users.noreply.github.com>
2024-12-22 12:01:29 +0100

    Fonctionalité : Appareil photo

--- src/app/native-feature/native-feature.page.ts
+++ src/app/native-feature/native-feature.page.ts
@@ -1,16 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 
 @Component({
   selector: 'app-native-feature',
   templateUrl: './native-feature.page.html',
   styleUrls: ['./native-feature.page.scss'],
-  standalone:false
 })
-export class NativeFeaturePage implements OnInit {
+export class NativeFeaturePage {
+  photo: string | null | undefined = null;
 
-  constructor() { }
 
-  ngOnInit() {
+  constructor() {}
+
+  async takePhoto() {
+    try {
+      const image = await Camera.getPhoto({
+        quality: 90,
+        allowEditing: false,
+        resultType: CameraResultType.DataUrl, // Retourne la photo sous forme de Data URL
+        source: CameraSource.Camera, // Utilise l'appareil photo
+      });
+      this.photo = image.dataUrl;
+    } catch (error) {
+      console.error('Erreur lors de la prise de photo :', error);
+    }
   }
 
+  async chooseFromGallery() {
+    try {
+      const image = await Camera.getPhoto({
+        quality: 90,
+        allowEditing: false,
+        resultType: CameraResultType.DataUrl,
+        source: CameraSource.Photos, // Utilise la galerie
+      });
+      this.photo = image.dataUrl;
+    } catch (error) {
+      console.error('Erreur lors de la sélection de photo :', error);
+    }
+  }
 }
